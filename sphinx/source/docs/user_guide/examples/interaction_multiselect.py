c0cfd3eea2ddbe2ec4faea7270683f8598550305
Bryan Van de Ven <bryan@bokeh.org>
2020-08-17 20:20:47 -0700

    improve user guide widgets docs

--- sphinx/source/docs/user_guide/examples/interaction_multiselect.py
+++ sphinx/source/docs/user_guide/examples/interaction_multiselect.py
@@ -1,9 +1,10 @@
-from bokeh.io import output_file, show
-from bokeh.models import MultiSelect
-
-output_file("multi_select.html")
+from bokeh.io import show
+from bokeh.models import CustomJS, MultiSelect
 
 multi_select = MultiSelect(title="Option:", value=["foo", "quux"],
                            options=[("foo", "Foo"), ("bar", "BAR"), ("baz", "bAz"), ("quux", "quux")])
+multi_select.js_on_change("value", CustomJS(code="""
+    console.log('multi_select: active=' + this.value, this.toString())
+"""))
 
 show(multi_select)
